d9df8718e6f97c9e1de5252a5069c9a21fc59cef
Septent <kindle1910@gmail.com>
2023-12-16 15:35:56 +0300

    first commit

---
+++ Lvl1.Lecture2/src/CreateArray.java
@@ -0,0 +1,11 @@
+/*
+ * В методе main проинициализируй переменные intArray и doubleArray
+ * массивами соответствующих типов, размер которых равен 10.
+*/
+
+public class CreateArray {
+    public static void main(String[] args) throws Exception {
+        int[] intArray = new int[10];
+        double[] doubleArray = new double[10];
+    }
+}
