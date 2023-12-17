33bc3d1419826c04995992bc263ab32b807df416
Septent <kindle1910@gmail.com>
2023-12-18 00:50:29 +0300

    added Lvl2.Lecture 3-5

---
+++ Lvl2.Lecture3/src/ManyMethods.java
@@ -0,0 +1,51 @@
+/*
+ * Неплохо было бы иметь один метод для решения различных задач. У тебя есть возможность написать такой.
+ * Создай еще 9 методов universalMethod(). В сумме их должно получиться 10.
+ * Параметры, которые они должны принимать, придумай сам.
+ */
+
+public class ManyMethods {
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod(){
+
+    }
+    
+    public static void universalMethod(int value){
+
+    }
+    
+    public static void universalMethod(String name){
+
+    }
+    
+    public static void universalMethod(String name, int value){
+
+    }
+    
+    public static void universalMethod(boolean isClose){
+
+    }
+    
+    public static void universalMethod(double someDouble){
+
+    }
+    
+    public static void universalMethod(int value, double someDouble){
+
+    }
+    
+    public static void universalMethod(int value, double someDouble, boolean isClose){
+
+    }
+    
+    public static void universalMethod(int value, double someDouble, boolean isClose, String name){
+
+    }
+    
+    public static void universalMethod(int xValue, int yValue){
+
+    }
+}
