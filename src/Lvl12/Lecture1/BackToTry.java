832188a22a051eb5a6a7e2dd4a0c8555266bb2e3
Septent <kindle1910@gmail.com>
2024-01-28 20:06:51 +0300

    added Lvl12 Lecture 2

---
+++ src/Lvl12/Lecture1/BackToTry.java
@@ -0,0 +1,24 @@
+package Lvl12.Lecture1;
+
+import java.util.Scanner;
+
+/*
+ * Программа считывает с консоли строку и выводит ее на экран в верхнем регистре. Но оказывается, она не будет работать на старых
+ * версиях Java (ниже 7й версии). Не меняя функциональности программы, перепиши блок try-with-resources на обычный try-catch.
+ * Не забудь о вызове метода close() для объектов с внешними ресурсами.
+ */
+
+public class BackToTry {
+    public static void main(String[] args) {
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(System.in);
+            String line = scanner.nextLine();
+            System.out.println(line.toUpperCase());
+        } catch (Exception e) {
+            System.out.println("Something went wrong : " + e);
+        } finally {
+            if (scanner != null) scanner.close();
+        }
+    }
+}
