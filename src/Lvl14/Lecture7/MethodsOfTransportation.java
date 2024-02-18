9b538b987d1ab1fae07a73a3f76c7f4c51000114
Septent <kindle1910@gmail.com>
2024-02-18 21:55:20 +0300

    added Lvl14 Lecture 7

---
+++ src/Lvl14/Lecture7/MethodsOfTransportation.java
@@ -0,0 +1,18 @@
+package Lvl14.Lecture7;
+
+/*
+ * Создай два интерфейса — Swimmable и Flyable.
+ * В будущем их можно будет использовать для обозначения классов, объекты которых умеют плавать или летать.
+ */
+
+public class MethodsOfTransportation {
+    
+}
+
+interface Flyable {
+    
+}
+
+interface Swimmable {
+
+}
